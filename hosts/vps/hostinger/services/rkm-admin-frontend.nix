16fe83627f957b5f15a715994e517aa4eaf308c1
maulanasdqn <maulanasdqn@gmail.com>
2026-01-11 03:01:29 +0700

    fix: remove acmeEmail from rkm-admin-frontend

--- hosts/vps/hostinger/services/rkm-admin-frontend.nix
+++ hosts/vps/hostinger/services/rkm-admin-frontend.nix
@@ -1,9 +1,8 @@
-{ acmeEmail, ... }:
+{ ... }:
 {
   services.rkm-admin-frontend = {
     enable = true;
     domain = "cms.rajawalikaryamulya.co.id";
     enableSSL = true;
-    acmeEmail = acmeEmail;
   };
 }

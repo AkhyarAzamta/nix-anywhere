fedc776839d95502f547a9d44fe5cdbb83cce243
maulanasdqn <maulanasdqn@gmail.com>
2026-01-10 21:43:28 +0700

    fix: config

---
+++ config.local.nix
@@ -0,0 +1,27 @@
+{
+  darwinUsername = "ms";
+  darwinHostname = "mrscraper";
+  darwinEnableTilingWM = true;
+
+  workstationUsername = "ms";
+  workstationHostname = "workstation";
+  workstationEnableTilingWM = true;
+
+  vpsHostingerUsername = "ms";
+  vpsHostingerHostname = "msdqn";
+  vpsHostingerIP = "72.62.125.38";
+  vpsHostingerGateway = "72.62.125.254";
+
+  acmeEmail = "maulanasdqn@gmail.com";
+
+  vpsDigitalOceanUsername = "ms";
+  vpsDigitalOceanHostname = "droplet";
+
+  enableLaravel = true;
+  enableRust = true;
+  enableVolta = true;
+
+  sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdLKnxrQl735W+ANR4dnWTrNEMmrIzv7TioI0teJmMZ ms@computer"
+  ];
+}
